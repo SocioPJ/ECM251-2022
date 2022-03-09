a0928cfb48f265e8b28aad4f3f2ae6826bdaaa32
SocioPJ <20.00704-3@maua.br>
2022-03-09 11:09:18 -0300

    Criação de Classes com Java

---
+++ Teoria/Aula04-INTRO-POO/src/Caneta.java
@@ -0,0 +1,12 @@
+public class Caneta {
+    //Caracteristica da caneta - seus atributos
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+
+    //Comportamentos das canetas - seus Métodos
+    void Escrever(String texto){
+        System.out.println(texto);
+    }
+}
