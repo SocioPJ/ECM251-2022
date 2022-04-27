966d9163435d715a127dcab1c21a8a4f4267c1e2
SocioPJ <20.00704-3@maua.br>
2022-04-27 10:22:26 -0300

    Criado métodos para Conta

---
+++ GabaritoSimuladoT1/Gabarito/src/Conta.java
@@ -0,0 +1,28 @@
+public class Conta {
+    private int idConta;
+    private double saldo;
+    private static int totalContas = 0;
+    public Conta() {
+        this.saldo = 0;
+        this.idConta = totalContas;
+        totalContas++;
+    }
+    public boolean depositar(double valor) {
+        this.saldo += valor;
+        return true;
+    }
+    public boolean sacar(double valor) {
+        if (valor <= this.saldo) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    public boolean transferir(Conta destino, double valor) {
+        if (this.sacar(valor)) {
+            destino.depositar(valor);
+            return true;
+        }
+        return false;
+    }
+}
