2b024e5c3780f73be8046b943a478194095661be
SocioPJ <20.00704-3@maua.br>
2022-05-05 11:58:21 -0300

    implementando lagarto spock

---
+++ Teoria/Aula11 - Polimorfismo/Aula11/src/Spock.java
@@ -0,0 +1,10 @@
+public class Spock extends Jogada {
+    public Spock() {
+        super(EnumJogadas.TESOURA,EnumJogadas.PEDRA);
+    }
+
+    @Override
+    public EnumJogadas getTipo() {
+        return EnumJogadas.PAPEL;
+    }
+}
