e2a0f16ccb90e4b3fbe8e9ded248e682802219ad
SocioPJ <20.00704-3@maua.br>
2022-08-11 12:56:49 -0300

    update todas classes

--- Teoria/Aula15 - POO/classes.py
+++ Teoria/Aula15 - POO/classes.py
@@ -1,5 +1,5 @@
 from item import Item
-#from carrinho import Carrinho
+from carrinho import Carrinho
 item1 = Item(
     preço = 350,
     nome = 'Dark Souls 3')
@@ -9,10 +9,14 @@ item2 = Item(
     nome = 'Elden Ring',
     descrição = 'Jogo de RPG Mundo aberto')
 
-carrinho1 = Carrinho
+item3 = Item(
+    preço = 100,
+    nome = 'Elden Ring',
+    descrição = 'Jogo de RPG Mundo aberto')
+
+carrinho1 = Carrinho()
 carrinho1.adicionar_item(item1)
 carrinho1.adicionar_item(item2)
-#print(carrinho1.get_quantidade_itens())
-
-print (item1.get_nome())
-print(item1)
+print(item1 == item2)
+print(item2 == item3)
+print(item2 == 'Jogo de RPG Mund Aberto')
