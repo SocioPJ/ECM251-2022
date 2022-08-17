cf64ea70eba5349a922a085190bf658acaf8326a
SocioPJ <20.00704-3@maua.br>
2022-08-17 11:11:12 -0300

    update

--- Teoria/Aula16/main.py
+++ Teoria/Aula16/main.py
@@ -3,16 +3,16 @@ from modelos.documentos import Document, EMail, Book
 
 def run_system():
     doc1 = Document()
-    doc2 = EMail(authors = ['João', 'Maria'])
-    doc3 = Book(authors = ['Murilo Zanini'])
-    print(doc2.get_authors())
-    print(doc3)
+    doc2 = EMail(authors = ['João'], to = 'jpsocio@hotmail.com')
+    doc3 = Book(authors = ['Murilo Zanini'], title = 'Python para Zumbis')
+    print(doc2)
+    # print(doc3)
 
 if __name__ == '__main__':
 
-    planta1 = Arvore(nome='Pau-de-laranja')
-    planta2 = Alface(nome='Alface')
+    # planta1 = Arvore(nome='Pau-de-laranja')
+    # planta2 = Alface(nome='Alface')
 
-    print(planta1.ola())
-    print(planta2.ola())
+    # print(planta1.ola())
+    # print(planta2.ola())
     run_system()
--- Teoria/Aula16/modelos/documentos.py
+++ Teoria/Aula16/modelos/documentos.py
@@ -9,10 +9,30 @@ class Document:
         return f'Document: Authors: {self._authors}, Date: {self._date}'
     def get_authors(self):
         return self._authors
+    def get_date(self):
+        return self._date
+    def add_author(self,author):
+        self._authors.append(author)
     
 
 class EMail(Document):
-    pass
+    def __init__(self, to ,subject = "", authors=[], date=date.today()):
+        super().__init__(authors, date)
+        self._to = to
+        self._subject = subject
+    def get_subject(self):
+        return self._subject
+    def get_to(self):
+        return self._to
+    def __str__(self):
+        return f'EMail : to:{self._to}, subject:{self._subject} {super().__str__()}'
 
 class Book(Document):
-    pass
+    def __init__(self,title, authors=[], date=date.today()):
+        super().__init__(authors, date)
+        self._title = title
+    def get_title(self):
+        return self._title
+    def __str__(self):
+        return f'Book: Title: {self._title} - {super().__str__()}'
+    
