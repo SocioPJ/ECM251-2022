367455fffe54af225dcb9943e4950c46f17ad1bb
SocioPJ <20.00704-3@maua.br>
2022-08-25 11:32:43 -0300

    update comida.py

--- ProjetoLoja - Python/comida.py
+++ ProjetoLoja - Python/comida.py
@@ -7,16 +7,15 @@ def __init__ (self,peso,origem,alergia,tipo): # Construtor
 def __str__ (self): # ToString
     return f'Peso: {self.peso}, Origem: {self.origem}, Alergia: {self.alergia}, Tipo: {self.tipo}'
 
-def get_peso():
+def get_peso(self):
     return self.peso
 
-def get_tipo():
+def get_tipo(self):
     return self.tipo
 
-def get_alergia():
+def get_alergia(self):
     return self.alergia
 
-def get_origem():
+def get_origem(self):
     return self.origem
-
 
