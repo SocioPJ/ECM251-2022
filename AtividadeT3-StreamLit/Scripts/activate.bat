54183ba5319582b855ee5019ec3edc20a6f7399c
SocioPJ <20.00704-3@maua.br>
2022-09-28 10:31:50 -0300

    Criado pasta teste

--- AtividadeT3-StreamLit/Scripts/activate.bat
+++ AtividadeT3-StreamLit/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Maua\Programação\ECM251-2022\AtividadeT3-StreamLit
+set VIRTUAL_ENV=E:\ECM251-2022\AtividadeT3-StreamLit
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(AtividadeT3-StreamLit) 
 
 :END
 if defined _OLD_CODEPAGE (
