32abfb1c083f15d1cb071b00c8b00a40b5b3d25d
SocioPJ <20.00704-3@maua.br>
2022-10-28 17:51:16 -0300

    criado arquivo __init__.py em dao

--- AtividadeT4-SQLite/tests/teste_controller.py
+++ AtividadeT4-SQLite/tests/teste_controller.py
@@ -3,8 +3,10 @@ from src.models.product import Product
 
 controller = ProductController()
 
-produto1 = Product("blabla",19.99,"blabla")
+produto1 = Product(1,"blabla",19.99,"blabla")
 print(produto1.id)
 print(produto1.name)
 print(produto1.price)
 print(produto1.url)
+
+print(controller.inserir_item(produto1))
