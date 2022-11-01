5e0f1effc68bb62529b024bfdb00ff7c3d464e3c
SocioPJ <20.00704-3@maua.br>
2022-11-01 10:43:57 -0300

    implementado tabs cadastrar produtos

--- AtividadeT4-SQLite/src/pages/cadastrar_produto.py
+++ AtividadeT4-SQLite/src/pages/cadastrar_produto.py
@@ -9,67 +9,69 @@ from models.product import Product
 controller = ProductController()
 i = 0
 if st.session_state.zoro:
-    st.header("Cadastrar Produtos")
-    with st.container():
-        with st.form("entry_form", clear_on_submit=True):
-            id_input = st.number_input(
-                label = "Digite o id do novo produto",
-                min_value = 0,
-                key = 'id_input'
-                
-            )
-            
-            if id_input != "":
-                i+=1
-            
-            
-            name_input = st.text_input(
-                label= "Digite o nome do novo produto",
-                key = 'name_input'
-                )
-            
-            if name_input != "":
-                i+=1
-        
-            
-            
-            price_input = st.number_input(
-                label = "Digite o preço do novo produto",
-                min_value= 0.01,
-                max_value= 1000000.00,
-                step= 0.01,
-                format='%.2f',
-                key = 'price_input'
-                )
-            if price_input != "":
-                i+=1
-            
-            
-            url_input = st.text_input(
-                label = "Digite o link da imagem do novo produto",
-                key = 'url_input'
-                )
-            if url_input != "":
-                i+=1
-            
-    
-                
-            if st.form_submit_button("Cadastrar"):
-                if i == 4:
-                    controller.inserir_item(Product(st.session_state["id_input"],st.session_state["name_input"],st.session_state["price_input"],st.session_state["url_input"]))
-                    # st.experimental_rerun()
-                    print(st.session_state['id_input'])
-                    st.write(f'id: {st.session_state["id_input"]}')
-                    st.write(f'name: {st.session_state["name_input"]}')
-                    st.write(f'price: {st.session_state["price_input"]}')
-                    st.write(f'url: {st.session_state["url_input"]}')       
-                    st.success("Produto cadastrado com sucesso")
+    st.title("Produtos")
+    tab1, tab2 = st.tabs(["Cadastrar", "Visualizar"])
+    with tab1:
+        with st.container():
+            with st.form("entry_form", clear_on_submit=True):
+                id_input = st.number_input(
+                    label = "Digite o id do novo produto",
+                    min_value = 0,
+                    key = 'id_input'
                     
+                )
                 
-                # Problema : Nao to conseguindo adicionar dois itens de uma vez sem q eu tenha q mudar alguma coisa no codigo
-                            
-                else:
-                    st.warning("Preencha todos os campos!")
+                if id_input != "":
+                    i+=1
+                
+                
+                name_input = st.text_input(
+                    label= "Digite o nome do novo produto",
+                    key = 'name_input'
+                    )
+                
+                if name_input != "":
+                    i+=1
+            
+                
+                
+                price_input = st.number_input(
+                    label = "Digite o preço do novo produto",
+                    min_value= 0.01,
+                    max_value= 1000000.00,
+                    step= 0.01,
+                    format='%.2f',
+                    key = 'price_input'
+                    )
+                if price_input != "":
+                    i+=1
+                
+                
+                url_input = st.text_input(
+                    label = "Digite o link da imagem do novo produto",
+                    key = 'url_input'
+                    )
+                if url_input != "":
+                    i+=1
+                
+        
+                    
+                if st.form_submit_button("Cadastrar"):
+                    if i == 4:
+                        controller.inserir_item(Product(st.session_state["id_input"],st.session_state["name_input"],st.session_state["price_input"],st.session_state["url_input"]))
+                        # st.experimental_rerun()
+                        print(st.session_state['id_input'])
+                        st.write(f'id: {st.session_state["id_input"]}')
+                        st.write(f'name: {st.session_state["name_input"]}')
+                        st.write(f'price: {st.session_state["price_input"]}')
+                        st.write(f'url: {st.session_state["url_input"]}')       
+                        st.success("Produto cadastrado com sucesso")
+                        
+                    
+                    # Problema : Nao to conseguindo adicionar dois itens de uma vez sem q eu tenha q mudar alguma coisa no codigo
+                                
+                    else:
+                        st.warning("Preencha todos os campos!")
                 
         
                     
