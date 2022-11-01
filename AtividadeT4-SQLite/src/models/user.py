4ac464e07ff32ff4f0be054038ec34a1c92b9ed8
SocioPJ <20.00704-3@maua.br>
2022-11-01 16:06:16 -0300

    metodos controller usuario testados e funcionando

--- AtividadeT4-SQLite/src/models/user.py
+++ AtividadeT4-SQLite/src/models/user.py
@@ -1,7 +1,8 @@
 class User():
-    def __init__(self,name ,email, password) -> None:
+    def __init__(self,id,name ,email, password) -> None:
+        self.id = id
         self.name = name
         self.email = email
         self.password = password
     def __str__(self) -> str:
-        return f'User(name:{self.name}, email:{self.email}, password:{self.password})'
+        return f'User(id:{self.id}, name:{self.name}, email:{self.email}, password:{self.password})'
---
+++ AtividadeT4-SQLite/src/teste_controller_usuario.py
@@ -0,0 +1,24 @@
+from controllers.user_controller import UserController
+from models.user import User
+
+controller = UserController()
+
+controller.deletar_item(0)
+controller.deletar_item(1)
+controller.deletar_item(2)
+controller.deletar_item(3)
+controller.deletar_item(4)
+controller.deletar_item(5)
+controller.deletar_item(6)
+controller.deletar_item(7)
+controller.deletar_item(8)
+controller.deletar_item(9)
+controller.deletar_item(10)
+controller.inserir_usuario(User(0,'joao','jpsocio@hotmail.com','arroz'))
+controller.inserir_usuario(User(1,'ana','ana@hotmail.com','cocada'))
+controller.inserir_usuario(User(2,'murilo','murilo@hotmail.com','farofa'))
+controller.deletar_item(0)
+print(controller.pegar_todos_usuarios())
+for usuario in controller.pegar_todos_usuarios():
+    print(usuario.id)
+    
